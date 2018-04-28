06682b009301cafc0f7d733c9c1347035494f93d
alex123bob <alex123bob@126.com>
2018-04-28 13:19:52 +0800

    Remove duplicate slash, fix error when retrieving encryptedData. Adding dev API_SERVER for local debugging.

--- src/mixins/server_config.js
+++ src/mixins/server_config.js
@@ -2,4 +2,5 @@ import wepy from 'wepy'
 
 export default class ServerConfig extends wepy.mixin {
     API_SERVER = 'https://dqjczs.sinaapp.com/wechatminiprogramme/'
+    // API_SERVER = 'http://localhost/dqjczs/1/wechatminiprogramme/'
 }
